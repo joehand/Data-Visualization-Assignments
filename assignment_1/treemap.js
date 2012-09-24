5401f01d74b43976dce57fef41a7769e35eb13d3
Joe Hand <joe@joehand.org>
2012-09-24 07:22:00 -0700

    Revert "Using underscore.nest to dynamically nest data."
    This reverts commit f85bdc8fc29cc4d5e8b02abf5fa11ff49f7260e0.

--- assignment_1/treemap.js
+++ assignment_1/treemap.js
@@ -27,7 +27,7 @@ var treemap = d3.layout.treemap()
     .size([width, height])
     .value(function(d) { return d.episode_count; });
 
-var svg = d3.select("#chart").append("svg")
+var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -37,7 +37,7 @@ var svg = d3.select("#chart").append("svg")
 	
 d3.json('tv-data.json', function(json) {
 	
-	//Create a duration variable for each show
+	
  	json.forEach(function(d) {
     	d.started_broadcasting = format.parse(d.started_broadcasting);
 		if (d.finished_broadcasting !== '')
@@ -48,15 +48,53 @@ d3.json('tv-data.json', function(json) {
 	    d.duration = new Date(d.finished_broadcasting - d.started_broadcasting).getTime();
   	});
 	
+	var tvseries = d3.nest()
+	      .key(function(d) { return d.country_of_origin.toLowerCase(); })
+		  .key(function(d) { return d.genre.toLowerCase(); })
+	      .sortKeys(d3.ascending)
+	      .map(json);
 	
 	var data = {
-		name: 'tv shows',
-		children: _.nest(json, [ 'country_of_origin', 'genre']).children
+		'name' : 'tv shows',
+		'children': []
 	};
+
+
+	//go down to show leaf and remake object to fit treemap data needs
+	$.each(tvseries, function(i, country) {
+		
+		var countryName = '';
+		var newCountry = {
+			'name' : '',
+			'children': []
+		};
+		
+		$.each(country, function(j, genre) {
+			
+			var genreName = '';
+			var newGenre = {
+				'name' : '',
+				'children': []
+			};
+			
+			$.each(genre, function(k, show) {
+				genreName = show.genre;
+				countryName = show.country_of_origin
+				show.name = show.program_name;
+				newGenre.children.push(show);
+			});
+			newGenre.name = genreName;
+			newCountry.children.push(newGenre);				
+		});		
+		
+		newCountry.name = countryName;
+		data.children.push(newCountry);		
+	});
 	
+		
 	console.log(data);
 	
-	//Add the cells to the treemap
+	
 	var cell = svg.data([data]).selectAll("g")
       .data(treemap)
     .enter().append("g")
@@ -73,9 +111,40 @@ d3.json('tv-data.json', function(json) {
 	      .attr("y", function(d) { return d.dy / 2; })
 	      .attr("dy", ".35em")
 	      .attr("text-anchor", "middle")
-	      .text(function(d) { return d.children ? null : d.data.program_name; });    
+	      .text(function(d) { return d.children ? null : d.data.name; });
+
+	d3.select("#episodes").on("click", function() {
+	   
+	    cell
+	        .data(treemap.value(function(d) { return d.episode_length; }))
+			.transition()
+			  .duration(1500)
+		    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+			.select('rect')
+			      .attr("width", function(d) { return d.dx; })
+			      .attr("height", function(d) { return d.dy; });
+
+	    d3.select("#episodes").classed("active", true);
+	    d3.select("#duration").classed("active", false);
+	  });
+
+
+	d3.select("#duration").on("click", function() {
+
+	    cell
+	        .data(treemap.value(function(d) { return d.duration; }))
+			.transition()
+			  .duration(1500)
+		    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+			.select('rect')
+			      .attr("width", function(d) { return d.dx; })
+			      .attr("height", function(d) { return d.dy; });
+				
+	    d3.select("#episodes").classed("active", false);
+	    d3.select("#duration").classed("active", true);
+	  });
+    
 	
 });
 
-	
 	
